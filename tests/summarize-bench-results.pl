fbff84ef494de44780b9c3a113ec5e32863c922b
Mark Lentczner <markl@glyphic.com>
2010-01-01 02:38:05 +0000

    add bench summary script

---
+++ tests/summarize-bench-results.pl
@@ -0,0 +1,14 @@
+#!/bin/perl
+
+
+while(<>) {
+    if (/^benchmarking (.*)$/) {
+        $name = $1;
+    }
+    if (/^mean:\s*([\d.]*) (\w*),/) {
+        $mean = 1 * $1;
+        $units = $2;
+        
+        printf "%-30s %10.2f %2s\n", $name, $mean, $units;
+    }
+}
