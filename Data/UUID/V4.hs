df517b3b1630d816247530574a82c63de16cb6e8
Antoine Latter <aslatter@gmail.com>
2012-06-14 02:04:44 +0000

    Call out random instance with its own module

---
+++ Data/UUID/V4.hs
@@ -0,0 +1,28 @@
+{- |
+   Module      : Data.UUID.V4
+   Copyright   : (c) 2012 Antoine Latter
+
+   License     : BSD-style
+
+   Maintainer  : aslatter@gmail.com
+   Stability   : experimental
+   Portability : portable
+
+   This module implements Version 4 UUIDs as specified
+   in RFC 4122.
+
+   These UUIDs are generated from a psuedo-random generator.
+   We use the System.Random 'R.StdGen' as our random source.
+
+   All of the logic is encapsulated in the 'R.Random' instance
+   for the UUID type, so you are free to use the random generator
+   of your choice.
+-}
+module Data.UUID.V4 (nextRandom) where
+
+import Data.UUID
+import qualified System.Random as R
+
+nextRandom :: IO UUID
+nextRandom = R.randomIO
+
