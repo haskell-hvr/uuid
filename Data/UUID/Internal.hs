5f00ffd5a0393d6f847e8d1b172cb2c8ee4d55f3
Antoine Latter <aslatter@gmail.com>
2013-10-19 18:51:58 +0000

    Storable instance now goes through UnpackedUUID
    The instance is faster and more correct.

--- Data/UUID/Internal.hs
+++ Data/UUID/Internal.hs
@@ -462,14 +462,39 @@ instance Read UUID where
 
 instance Storable UUID where
     sizeOf _ = 16
-    alignment _ = 1 -- UUIDs are stored as individual octets
+    alignment _ = 4
 
     peekByteOff p off =
-      mapM (peekByteOff p) [off..(off+15)] >>= return . fromJust . fromList 
+      pack <$>
+       (UnpackedUUID
+             <$> peekByteOff p off -- Word32
+             <*> peekByteOff p (off+4) -- Word16
+             <*> peekByteOff p (off+6) -- Word16
+             <*> peekByteOff p (off+8) -- Word8
+             <*> peekByteOff p (off+9) -- Word8
+             <*> peekByteOff p (off+10) -- Word8
+             <*> peekByteOff p (off+11) -- Word8
+             <*> peekByteOff p (off+12) -- Word8
+             <*> peekByteOff p (off+13) -- Word8
+             <*> peekByteOff p (off+14) -- Word8
+             <*> peekByteOff p (off+15) -- Word8
+        )
 
     pokeByteOff p off u =
-      sequence_ $ zipWith (pokeByteOff p) [off..] (toList u)
-
+        case unpack u of
+          (UnpackedUUID x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 x10) ->
+              do
+                pokeByteOff p off x0
+                pokeByteOff p (off+4) x1
+                pokeByteOff p (off+6) x2
+                pokeByteOff p (off+8) x3
+                pokeByteOff p (off+9) x4
+                pokeByteOff p (off+10) x5
+                pokeByteOff p (off+11) x6
+                pokeByteOff p (off+12) x7
+                pokeByteOff p (off+13) x8
+                pokeByteOff p (off+14) x9
+                pokeByteOff p (off+15) x10
 
 instance Binary UUID where
     put (UUID w0 w1 w2 w3) =
