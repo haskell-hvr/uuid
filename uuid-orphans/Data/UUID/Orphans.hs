bcc0f735c65f2c3b1c7cc6fc4889652cc1bf8334
David Fox <dsf@seereason.com>
2015-09-20 09:23:39 -0700

    Add uuid-orphans package

---
+++ uuid-orphans/Data/UUID/Orphans.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Data.UUID.Orphans () where
+
+import Data.SafeCopy (base, deriveSafeCopy)
+import Data.Text as T (pack, unpack)
+import Data.UUID (UUID, toString, fromString)
+import Web.Routes.PathInfo
+
+$(deriveSafeCopy 0 'base ''UUID)
+
+instance PathInfo UUID where
+  toPathSegments = (:[]) . T.pack . toString
+  fromPathSegments = pToken (const ("UUID" :: String)) checkUUID
+    where checkUUID txt = fromString (T.unpack txt)
