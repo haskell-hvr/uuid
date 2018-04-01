a74bdf554506a4987554f426a86d8afbb0fda83c
Herbert Valerio Riedel <hvr@gnu.org>
2018-04-01 18:34:35 +0200

    reindent

--- uuid-types/Data/UUID/Types.hs
+++ uuid-types/Data/UUID/Types.hs
@@ -14,24 +14,25 @@ See <http://en.wikipedia.org/wiki/UUID> for the general idea.
 See <http://tools.ietf.org/html/rfc4122 RFC 4122> for the specification.
 
 -}
-module Data.UUID.Types(UUID
-                ,toString
-                ,fromString
-                ,toText
-                ,fromText
-                ,toASCIIBytes
-                ,fromASCIIBytes
-                ,toLazyASCIIBytes
-                ,fromLazyASCIIBytes
-                ,toByteString
-                ,fromByteString
-                ,toWords
-                ,fromWords
-                ,toWords64
-                ,fromWords64
-                ,null
-                ,nil
-                ) where
+module Data.UUID.Types
+    ( UUID
+    , toString
+    , fromString
+    , toText
+    , fromText
+    , toASCIIBytes
+    , fromASCIIBytes
+    , toLazyASCIIBytes
+    , fromLazyASCIIBytes
+    , toByteString
+    , fromByteString
+    , toWords
+    , fromWords
+    , toWords64
+    , fromWords64
+    , null
+    , nil
+    ) where
 
 import           Data.UUID.Types.Internal
 import           Prelude                  ()
