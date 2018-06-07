e56db508e2ffe4eedb2009ea8af5097535331ba6
David Fox <dsf@seereason.com>
2018-06-06 19:26:20 -0700

    gunfold and gfoldl implementations for instance Data UUID

--- uuid-types/Data/UUID/Types/Internal.hs
+++ uuid-types/Data/UUID/Types/Internal.hs
@@ -582,7 +582,8 @@ instance Binary UUID where
 -- I want when used with the HStringTemplate library.
 instance Data UUID where
     toConstr uu  = mkConstr uuidType (show uu) [] (error "fixity")
-    gunfold _ _  = error "gunfold"
+    gunfold k z _ = k (k (z UUID))
+    gfoldl k z (UUID a1 a2) = ((z UUID `k` a1) `k` a2)
     dataTypeOf _ = uuidType
 
 uuidType :: DataType
