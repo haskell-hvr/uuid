6814816db48fe9298f3dad3987cc5352465a0100
Laurent Rene de Cotret <l.rdc@socivolta.com>
2024-08-19 11:17:24 -0400

    Add support for UUIDv7

--- uuid/tests/TestUUID.hs
+++ uuid/tests/TestUUID.hs
@@ -6,11 +6,14 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Char (ord)
 import Data.List (nub, (\\))
 import Data.Maybe
+import qualified Data.Time as Time
+import qualified Data.Time.Clock.System as Time
 import Data.Word
 import qualified Data.UUID as U
 import qualified Data.UUID.V1 as U
 import qualified Data.UUID.V3 as U3
 import qualified Data.UUID.V5 as U5
+import qualified Data.UUID.V7 as U7
 
 import Test.QuickCheck ( Arbitrary(arbitrary), choose )
 import Test.Tasty ( TestTree, testGroup, defaultMain )
@@ -65,6 +68,32 @@ test_v5 =
     where name = map (fromIntegral . ord) "www.widgets.com" :: [Word8]
           uV5 = fromJust $ U.fromString "21f7f8de-8051-5b89-8680-0195ef798b6a"
 
+test_v7 :: Test
+test_v7 = testGroup
+    "version 7"
+    [ testGroup
+        "generate"
+        [ testCase "works" $ do
+            uuid <- U7.generate
+            length (U.toString uuid) @?= 36
+        ],
+      testGroup
+        "build"
+        [ testCase "works with nil" $ do
+            let epoch = Time.MkSystemTime 0 0
+            let uuid = U7.build epoch 0 0
+            U.toString uuid @?= "00000000-0000-7000-8000-000000000000",
+          testCase "works with test vector" $ do
+            -- https://datatracker.ietf.org/doc/html/rfc9562#name-example-of-a-uuidv7-value
+            let time =
+                  Time.utcToSystemTime
+                    . Time.localTimeToUTC (Time.hoursToTimeZone (-5))
+                    . Time.LocalTime (Time.fromGregorian 2022 2 22)
+                    $ Time.TimeOfDay 14 22 22
+            let uuid = U7.build time 0xcc3 0x18C4DC0C0C07398F
+            U.toString uuid @?= "017f22e2-79b0-7cc3-98c4-dc0c0c07398f"
+        ]
+    ]
 
 prop_randomsValid :: Test
 prop_randomsValid = testProperty "Random valid" randomsValid
@@ -101,7 +130,8 @@ main = do
         test_null,
         test_v1 v1s,
         test_v3,
-        test_v5
+        test_v5,
+        test_v7
         ]
      , [ prop_randomsValid,
          prop_v3NotNull,
